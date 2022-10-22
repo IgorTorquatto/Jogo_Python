18760d9d89f3e991cff1a35673b4ee619675957f
Igor <igor.torquato.20@gmail.com>
2022-10-22 11:27:13 -0300

    Alterando menu e adicionando animação

--- menu.py
+++ menu.py
@@ -1,42 +1,113 @@
-from tkinter import *
-import tkinter as tk
-import time
+def menu():
 
-#Constantes:
-WIDTH = 480
-HEIGHT = 300
-xvel = 1
-yvel = 1
+    class Button():
+        def __init__(self, image, pos, text_input, font, base_color, hovering_color):
+            self.image = image
+            self.x_pos = pos[0]
+            self.y_pos = pos[1]
+            self.font = font
+            self.base_color, self.hovering_color = base_color, hovering_color
+            self.text_input = text_input
+            self.text = self.font.render(self.text_input, True, self.base_color)
+            if self.image is None:
+                self.image = self.text
+            self.rect = self.image.get_rect(center=(self.x_pos, self.y_pos))
+            self.text_rect = self.text.get_rect(center=(self.x_pos, self.y_pos))
 
-window = Tk()
-window.title("Escape from Earth")
+        def update(self, screen):
+            if self.image is not None:
+                screen.blit(self.image, self.rect)
+            screen.blit(self.text, self.text_rect)
 
-canvas = Canvas(window,width=WIDTH,height=HEIGHT)
-canvas.pack()
+        def checkForInput(self, position):
+            if position[0] in range(self.rect.left, self.rect.right) and position[1] in range(self.rect.top,
+                                                                                              self.rect.bottom):
+                return True
+            return False
 
-background_photo = PhotoImage(file='imagens/fundo.png')
-background = canvas.create_image(0,0,image=background_photo,anchor=NW)
-
-tk.Button(window, text='Jogar',font=('Arial',14)).place(x=200, y=80)
-tk.Button(window, text='Instruções',font=('Arial',14)).place(x=200, y=130)
-tk.Button(window, text='Dificuldade',font=('Arial',14)).place(x=200, y=180)
-
-photo_image = PhotoImage(file='imagens/nave.png')
-my_image = canvas.create_image(0,0,image=photo_image,anchor=NW)
-
-image_width = photo_image.width()
-image_height = photo_image.height()
-
-while True:
-    coordenadas = canvas.coords(my_image)
-    if(coordenadas[0]>=WIDTH-image_width or coordenadas[0]<0):
-        xvel = -xvel
-    if (coordenadas[1] >= HEIGHT - image_height or coordenadas[1] < 0):
-        yvel = -yvel
-    canvas.move(my_image,xvel,yvel)
-    window.update()
-    time.sleep(0.01)
+        def changeColor(self, position):
+            if position[0] in range(self.rect.left, self.rect.right) and position[1] in range(self.rect.top,
+                                                                                              self.rect.bottom):
+                self.text = self.font.render(self.text_input, True, self.hovering_color)
+            else:
+                self.text = self.font.render(self.text_input, True, self.base_color)
 
 
+    pygame.init()
 
-window.mainloop()
+    SCREEN = pygame.display.set_mode((1000, 650)) #1280,720
+    pygame.display.set_caption("Menu")
+
+    BG = pygame.image.load("imagens/fundo - Copia.png")
+
+    def get_font(size):  # Returns Press-Start-2P in the desired size
+        return pygame.font.Font("assets/font.ttf", size)
+
+    def play():
+        main()
+
+    def options():
+        while True:
+            OPTIONS_MOUSE_POS = pygame.mouse.get_pos()
+
+            SCREEN.fill("white")
+
+            OPTIONS_TEXT = get_font(45).render("This is the OPTIONS screen.", True, "Black")
+            OPTIONS_RECT = OPTIONS_TEXT.get_rect(center=(640, 260))
+            SCREEN.blit(OPTIONS_TEXT, OPTIONS_RECT)
+
+            OPTIONS_BACK = Button(image=None, pos=(640, 460),
+                                  text_input="BACK", font=get_font(75), base_color="Black", hovering_color="Green")
+
+            OPTIONS_BACK.changeColor(OPTIONS_MOUSE_POS)
+            OPTIONS_BACK.update(SCREEN)
+
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    sys.exit()
+                if event.type == pygame.MOUSEBUTTONDOWN:
+                    if OPTIONS_BACK.checkForInput(OPTIONS_MOUSE_POS):
+                        main_menu()
+
+            pygame.display.update()
+
+    def main_menu():
+        while True:
+            SCREEN.blit(BG, (0, 0))
+
+            MENU_MOUSE_POS = pygame.mouse.get_pos()
+
+            MENU_TEXT = get_font(100).render("MENU", True, "#b68f40")
+            MENU_RECT = MENU_TEXT.get_rect(center=(540, 100))
+
+            PLAY_BUTTON = Button(image=pygame.image.load("assets/Play Rect.png"), pos=(540, 250),
+                                 text_input="JOGAR", font=get_font(75), base_color="#d7fcd4", hovering_color="White")
+            OPTIONS_BUTTON = Button(image=pygame.image.load("assets/Options Rect.png"), pos=(540, 400),
+                                    text_input="MANUAL", font=get_font(75), base_color="#d7fcd4",
+                                    hovering_color="White")
+            QUIT_BUTTON = Button(image=pygame.image.load("assets/Quit Rect.png"), pos=(540, 550),
+                                 text_input="SAIR", font=get_font(75), base_color="#d7fcd4", hovering_color="White")
+
+            SCREEN.blit(MENU_TEXT, MENU_RECT)
+
+            for button in [PLAY_BUTTON, OPTIONS_BUTTON, QUIT_BUTTON]:
+                button.changeColor(MENU_MOUSE_POS)
+                button.update(SCREEN)
+
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    sys.exit()
+                if event.type == pygame.MOUSEBUTTONDOWN:
+                    if PLAY_BUTTON.checkForInput(MENU_MOUSE_POS):
+                        play()
+                    if OPTIONS_BUTTON.checkForInput(MENU_MOUSE_POS):
+                        options()
+                    if QUIT_BUTTON.checkForInput(MENU_MOUSE_POS):
+                        pygame.quit()
+                        sys.exit()
+
+            pygame.display.update()
+
+    main_menu()
